26f11d33ddc509a9e6ee966d026e4912306d1b61
Medrine Mulindi <medrine.mulindi@gmail.com>
2024-07-13 20:49:24 +0300

    amends page title

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,3 @@
 export default function Page(){
-    return <p className="text-center mt-10 font-bold text-6xl text-sky-500">Invoices Page</p>
+    return <h1 className="text-center mt-10 font-bold text-3xl text-orange-500">Invoices Page</h1>
 }
