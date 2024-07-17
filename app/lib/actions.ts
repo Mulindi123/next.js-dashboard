75c45f517e77f6ea4bb418e6bc201c4750aeade3
Medrine Mulindi <medrine.mulindi@gmail.com>
2024-07-17 22:27:38 +0300

    updating a record

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -12,9 +12,11 @@ const FormSchema = z.object({
     date: z.string()
 });
 
-const CreateInvoice = FormSchema.omit({id:true, date:true})
+const CreateInvoice = FormSchema.omit({id:true, date:true});
+const UpdateInvoice = FormSchema.omit({id: true, date: true});
 
 export async function createInvoice(formData: FormData) {
+
   const { customerId, amount, status} = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
@@ -23,6 +25,7 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
+
   await sql `
     INSERT INTO invoices(customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
@@ -30,3 +33,23 @@ export async function createInvoice(formData: FormData) {
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 } 
+
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+   
+    const amountInCents = amount * 100;
+   
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+   
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+  }
