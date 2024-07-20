54791dce804e90efe31b8bfcc042b0e6f5cc917b
Medrine Mulindi <medrine.mulindi@gmail.com>
2024-07-20 23:01:19 +0300

    implements delete invoice

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -6,7 +6,7 @@ export default async function Page({ params }:{ params:{ id:string } }) {
     const id = params.id;
     const [invoice, customers] = await Promise.all([
         fetchInvoiceById(id),
-        fetchCustomers
+        fetchCustomers()
     ]);
 
   return (
