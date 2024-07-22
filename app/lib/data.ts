bd29a191e2fdd538eaa4e9db59b6446e2a8ce78b
Medrine Mulindi <medrine.mulindi@gmail.com>
2024-07-22 21:34:55 +0300

    adds not-found component

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -145,6 +145,7 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
+
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -162,6 +163,7 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
+    console.log(invoice)
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
