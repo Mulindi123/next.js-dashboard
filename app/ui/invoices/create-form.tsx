c34b194a14a36e26f87caddd2611675e42524407
Medrine Mulindi <medrine.mulindi@gmail.com>
2024-07-22 22:47:09 +0300

    form validation

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,5 +1,8 @@
+'use client';
+
+import { useActionState } from 'react';
 import { CustomerField } from '@/app/lib/definitions';
-import { createInvoice } from '@/app/lib/actions';
+import { createInvoice, State } from '@/app/lib/actions';
 import Link from 'next/link';
 import {
   CheckIcon,
@@ -10,8 +13,11 @@ import {
 import { Button } from '@/app/ui/button';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+  const initialState: State = { message: null, errors: {} };
+  const [state, formAction] = useActionState(createInvoice, initialState);
+
   return (
-    <form action={createInvoice}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
